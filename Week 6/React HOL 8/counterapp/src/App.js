549afa5bf84dac659185569854a51e65193b865e
Shreevalli Gundal <valligundal@gmail.com>
2026-07-20 18:16:24 +0530

    Add files via upload

---
+++ Week 6/React HOL 8/counterapp/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import CountPeople from "./CountPeople";
+
+function App() {
+
+  return (
+
+    <div>
+
+      <CountPeople />
+
+    </div>
+
+  );
+
+}
+
+export default App;
